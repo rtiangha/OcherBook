6965f630b2d3c233a6a16ad194cdb74064df6162
Chuck Coffing <clc@alum.mit.edu>
2012-11-30 22:26:59 -0700

    0.0.8 done; updated roadmap for 0.0.9 and 0.1

--- tools/put-kobo.sh
+++ tools/put-kobo.sh
@@ -8,6 +8,7 @@
     echo "cd /tmp"
     echo "lcd build"
     echo "put ocher"
+    echo "lcd .."
     echo "put DejaVuSerif.ttf"
     echo "put DejaVuSerif-Italic.ttf"
     echo "put DejaVuSerif-Bold.ttf"
@@ -16,12 +17,11 @@
 ) | ftp $OCHER_IP
 
 (
-    sleep 1
     echo "root"
     sleep 1
     echo "cd /tmp"
     echo "chmod +x ocher"
-    echo "killall nickel"
-    echo "killall ocher"
-    echo "nohup ./ocher -vv &"
+    #echo "killall nickel"
+    #echo "killall ocher"
+    #echo "nohup ./ocher -vv &"
 ) | telnet $OCHER_IP
