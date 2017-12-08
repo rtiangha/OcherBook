6554412aea3817006169a87b38d81f5be2923ccb
Chuck Coffing <clc@alum.mit.edu>
2017-12-07 21:41:01 -0700

    Parameterize Doxyfile and versions

--- ocher/ocher.h
+++ ocher/ocher.h
@@ -26,16 +26,6 @@
 #ifndef OCHER_APP_H
 #define OCHER_APP_H
 
-#ifndef OCHER_MAJOR
-#define OCHER_MAJOR 0
-#endif
-
-#ifndef OCHER_MINOR
-#define OCHER_MINOR 0
-#endif
-
-#ifndef OCHER_PATCH
-#define OCHER_PATCH 0
-#endif
+#include "version.h"
 
 #endif
