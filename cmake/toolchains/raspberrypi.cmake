7dda106da8167125271ed8312b0f64f46fdb44bc
Chuck Coffing <clc@alum.mit.edu>
2018-03-02 22:52:44 -0700

    Add some (incomplete) cross-compile toolchains

--- cmake/toolchains/raspberrypi.cmake
+++ cmake/toolchains/raspberrypi.cmake
@@ -1,42 +1,36 @@
 # Targets: Raspberry Pi
 # Requires: x86 Linux host.
 #
-# OcherBook defaults to expecting toolchains in a common location, with a
-# somewhat standardized naming scheme (although you can override this with the
-# OCHER_TOOLCHAIN_ROOT environment variable).
-#
 # The Raspberry Pi foundation has some prebuilt toolchains here:
 # 	https://github.com/raspberrypi/tools/tree/master/arm-bcm2708
-#
-# Summary instructions adapted for OcherBook:
 # 	$ git clone ...
+# 	$ TODO
+#
+# Install it to some common location with a subdirectory named after this file:
+#   $ mkdir -p ~/toolchains
 # 	$ TODO
 #
 # Finally, configure and build with the toolchain:
 # 	$ cd OcherBook
 # 	$ mkdir build
 # 	$ cd build
-# 	$ cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/raspberrypi.cmake ..
+#   $ cmake -DOCHER_TOOLCHAIN_ROOT=$HOME/toolchains -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/raspberrypi.cmake ..
 # 	$ make
 
 
 set(OCHER_TARGET raspberrypi)
 set(OCHER_TARGET_RASPBERRYPI TRUE)
 
-set(WITH_FREETYPE_DEFAULT On)
-set(WITH_FRAMEBUFFER_DEFAULT On)
+set(WITH_FREETYPE_DEFAULT ON)
+set(WITH_FRAMEBUFFER_DEFAULT ON)
+set(WITH_SYSTEM_LIBEV_DEFAULT OFF)
 
 set(CMAKE_SYSTEM_NAME Linux)
 
-if("$ENV{OCHER_TOOLCHAIN_ROOT}" STREQUAL "")
-	set(OCHER_TOOLCHAIN_ROOT "/opt/toolchains/arm-bcm2708hardfp-linux-gnuabi/")
-else()
-	set(OCHER_TOOLCHAIN_ROOT "$ENV{OCHER_TOOLCHAIN_ROOT}")
-endif()
-message("Using raspberrypi toolchain in ${OCHER_TOOLCHAIN_ROOT}")
-set(CMAKE_C_COMPILER      "${OCHER_TOOLCHAIN_ROOT}/bin/arm-bcm2708hardfp-linux-gnueabi-gcc")
-set(CMAKE_CXX_COMPILER    "${OCHER_TOOLCHAIN_ROOT}/bin/arm-bcm2708hardfp-linux-gnueabi-g++")
-set(CMAKE_FIND_ROOT_PATH  "${OCHER_TOOLCHAIN_ROOT}/arm-bcm2708hardfp-linux-gnueabi/")
+message("Using toolchain in ${OCHER_TOOLCHAIN_ROOT}/${OCHER_TARGET}")
+set(CMAKE_C_COMPILER      "${OCHER_TOOLCHAIN_ROOT}/${OCHER_TARGET}/bin/arm-bcm2708hardfp-linux-gnueabi-gcc")
+set(CMAKE_CXX_COMPILER    "${OCHER_TOOLCHAIN_ROOT}/${OCHER_TARGET}/bin/arm-bcm2708hardfp-linux-gnueabi-g++")
+set(CMAKE_FIND_ROOT_PATH  "${OCHER_TOOLCHAIN_ROOT}/${OCHER_TARGET}/arm-bcm2708hardfp-linux-gnueabi/")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
