621168b782cb7322c9ed4e45bdd922b735765adf
Reg Tiangha <reg@reginaldtiangha.com>
2022-06-29 11:48:50 -0600

    zlib-1.2.12

--- subprojects/zlib.cmake
+++ subprojects/zlib.cmake
@@ -4,8 +4,8 @@ if (WITH_SYSTEM_ZLIB)
     find_library(zlib REQUIRED)
 else()
     ExternalProject_add(zlib-external
-        URL http://zlib.net/zlib-1.2.11.tar.gz
-        URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+        URL http://zlib.net/zlib-1.2.12.tar.gz
+        URL_HASH SHA256=91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9
         DOWNLOAD_DIR ${TOP_SRCDIR}/subprojects/packagecache/
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND <SOURCE_DIR>/configure --static --prefix=<INSTALL_DIR>
